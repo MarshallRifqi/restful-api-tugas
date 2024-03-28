f6219fca143bb2dfa1a68b5ffee146e9b5a3993f
MarshallRifqi <marshallrifqi2@gmail.com>
2024-03-28 23:48:25 +0700

    inisiasi tugas api

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
+app.use(express.json());
+
+let data = []; // Untuk menyimpan data sementara
+
+// Route untuk menampilkan semua data
+app.get('/api/data', (req, res) => {
+    res.json(data);
+});
+
+app.listen(PORT, () => {
+    console.log('Server berjalan di http://localhost:${PORT}');
+});
